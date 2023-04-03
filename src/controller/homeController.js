bf8a8ad2b577238cdd3ec6e7b23b4937bbd9ef2f
t01292243272 <anh0982141649@gmail.com>
2023-04-03 10:42:35 +0700

    Create New User

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,13 @@
+import { name } from 'ejs';
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit-nodejs'
+});
+
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs")
 }
@@ -8,7 +18,24 @@ const handleUserPage = (req, res) => {
     return res.render("user.ejs")
 }
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+    let username = req.body.username
+
+    // simple query
+    connection.query(
+        'INSERT INTO users(email, password, username) VALUES (?,?,?)', [email, name, password],
+        function (err, results, fields) {
+            if (err) {
+                console.log(err);
+            }
+        }
+    );
+
+    return res.send("handleCreateNewUser")
+}
+
 module.exports = {
-    handleHelloWorld,
-    handleUserPage
+    handleHelloWorld, handleUserPage, handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -2,12 +2,22 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // config view engine
 configViewEngine(app);
+
+// -----------------------------
+// config body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+// -----------------------------
 
 // init web routes
 initWebRoutes(app);
