918ffd6d79aca2fa8bd7b39226f98fac2164fa67
t01292243272 <anh0982141649@gmail.com>
2023-04-18 23:42:55 +0700

    Mock APIs - Tao API de test

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
 
 const router = express.Router();
 
@@ -15,6 +16,10 @@ const initWebRoutes = (app) => {
     router.post("/delete-user/:id", homeController.handleDeleteUser)
     router.get("/update-user/:id", homeController.getUpdateUser)
     router.post("/users/update-user", homeController.handleUpdateUser)
+
+    // rest api
+    // POST--C, GET--R, PUT--U, DELETE--D
+    router.get("/api/test-api", apiController.testApi)
 
     return app.use("/", router)
 }
